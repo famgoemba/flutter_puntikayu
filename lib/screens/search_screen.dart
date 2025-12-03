bc9ca57f7c956f7446dd1733592e7c0d9c0b9ddd
Ahmad Farisi <ahmfarisi@gmail.com>
2025-12-03 11:20:43 +0700

    Update search_screen.dart

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_puntikayu/data/wisata_data.dart';
 import 'package:flutter_puntikayu/models/wisata_model.dart';
+import 'package:flutter_puntikayu/screens/detail_screen.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -69,7 +70,36 @@ class _SearchScreenState extends State<SearchScreen> {
               ),
             ),
           ),
+
           // HASIL PENCARIAN
+          Expanded(
+            child: ListView.builder(
+              itemCount: _filteredWisataList.length,
+              itemBuilder: (context, index) {
+                WisataModel wisataModel = _filteredWisataList[index];
+                return ListTile(
+                  title: Text(wisataModel.nama),
+                  subtitle: Text(wisataModel.instagram),
+                  leading: Image.asset(
+                    wisataModel.gambarUtama,
+                    width: 50,
+                    height: 50,
+                    fit: BoxFit.cover,
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return DetailScreen(wisataModel: wisataModel);
+                        },
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
